0e32da42280f530b43e8f8c5dc9d1c25fbf969fd
Abdallah M. Shnaino <abdallah.shnaino@gmail.com>
2022-03-27 03:23:28 +0300

    init

--- routes/friends.route.js
+++ routes/friends.route.js
@@ -3,6 +3,10 @@ const friendController = require('../controllers/friends.controller')
 const friends = friendController.friends
 
 let friendsRoute = express.Router()
+friendsRoute.use( ( request , response , next) => {
+    console.log('machine ip address ' , request.ip)
+    next()
+})
 friendsRoute.get('/' , friendController.getAllFriends)
 friendsRoute.get('/:friendId' ,friendController.getFriend)
 friendsRoute.post('' , (friendController.addNewFriend))
